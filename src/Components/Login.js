2462add5195cab738238b4447adab2e83f461db0
jhabar98 <125726549+jhabar98@users.noreply.github.com>
2023-08-28 07:11:19 +0530

    commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,6 +4,33 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
 function Login() {
+    function handlelogin() {
+        alert("success");
+        var password = document.getElementById('password').value;
+        var email = document.getElementById('email').value;
+        if ( email != ''&& password != '' )
+        console.log(email,password)
+         {
+          fetch('http://13.48.194.120:8000//registre/', {
+            method: 'POST',
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({
+              email: email,
+              password: password,
+              
+  
+            })
+          }).then(res => res.json())
+            .then(data => {
+              if (data.error) {
+              }
+              else {
+              }
+            })
+        }
+      }
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -17,7 +44,7 @@ function Login() {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
+          <Modal.Title>Login Here</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
@@ -25,19 +52,33 @@ function Login() {
             <Form.Group className="mb-3" controlId="inputemail">
               <Form.Label>Email address</Form.Label>
               <Form.Control
-                type="emai"
+                type="email"
+                placeholder="enter email"
+                autoFocus
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="inputpassword">
-              <Form.Label>Password</Form.Label>
+            //<Form.Group className="mb-3" controlId="inputpassword">
+              <Form.Label>PASSWORD</Form.Label>
               <Form.Control
                 type="password"
+                placeholder="enter password"
+                autoFocus
               />
             </Form.Group>
+            {/* <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group> */}
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleClose}>
+          {/* <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button> */}
+          <Button variant="primary" onClick={handlelogin}>
             SUBMIT
           </Button>
         </Modal.Footer>
@@ -45,4 +86,5 @@ function Login() {
     </>
   );
 }
+
 export default Login;
